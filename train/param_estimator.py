f618867a1cb211bd77116014b608c5fec7a73483
Sutirtha Biswas <67169634+Sutirtha-github@users.noreply.github.com>
2025-06-28 18:42:44 +0530

    Update param_estimator.py

--- train/param_estimator.py
+++ train/param_estimator.py
@@ -6,17 +6,40 @@ from data.data_utils import rabi_freq, detuning, mixangle, phonon_abs, phonon_em
 
 
 
-def train_inv_pinn_1(A0, v_c, T, D, Om_0, t_obs, sx_obs, sy_obs, 
-                    sz_obs, t_test, t_intervals=150, hidden_dim=32, 
-                    n_layers=4, epochs=301, lr=5e-3, plot_interval=50):
+def train_inv_pinn_1(A0, v_c, T, D, Om_0, t_obs, sx_obs, sy_obs, sz_obs, t_test, t_intervals=150, 
+                     hidden_dim=32, n_layers=4, epochs=301, lr=5e-3, plot_interval=50):
+
+    """
+    Estimate the value of system-bath coupling strength 'A' (in ps/K units) from the given 
+    noisy dataset, differential equations and initial boundary conditions.
+
+    Args:
+      A0: actual known value of coupling strength (ps/K)
+      v_c: cutoff frequency (1/ps)
+      T: bath temperature (K)
+      D: pulse duration (ps)
+      t_obs: time instances of the observed noisy dataset (ps)
+      sx_obs, sy_obs, sz_obs: noisy dataset
+      t_test: time points over the entire domain [0,D] for inferencing
+      t_intervals: # time points between [0,D]
+      hidden_dim: # units per hidden layer
+      n_layers: # hidden layers
+      epochs: # training iterations
+      lr: learning rate
+      plot_interval: frequency of displaying learning plots
+
+    Returns:
+      a, pinn: predicted value of A, trained pinn model
+    """
     
     torch.manual_seed(2021)
 
     eps = 1e-10
 
-    # define PINN and learnable parameter 'a'
+    # define PINN and additional learnable parameter 'a' to learn the system-bath coupling strength (in ps/K units)
     pinn = BlochNN(1, 3, hidden_dim, n_layers)
-    a = torch.nn.Parameter(0.1 * torch.ones(1, requires_grad=True))
+    a = torch.nn.Parameter(0.1 * torch.ones(1, requires_grad=True))    # initialized to 0.1
+                       
     optimiser = torch.optim.Adam(list(pinn.parameters()) + [a], lr=lr)
 
     t_physics = torch.linspace(0, D, t_intervals).view(-1, 1).requires_grad_(True)
@@ -27,6 +50,7 @@ def train_inv_pinn_1(A0, v_c, T, D, Om_0, t_obs, sx_obs, sy_obs,
     for i in range(epochs):
         optimiser.zero_grad()
 
+        # Compute the time dependent parameters over the entire time domain
         rabi_list = rabi_freq(t=t_physics, D=D, Om_0=Om_0)
         detuning_list = detuning(t=t_physics, D=D, Om_0=Om_0)
         L_list = torch.sqrt(rabi_list**2 + detuning_list**2) + eps
@@ -34,13 +58,16 @@ def train_inv_pinn_1(A0, v_c, T, D, Om_0, t_obs, sx_obs, sy_obs,
         ga_list = phonon_abs(v=L_list, A=a, v_c=v_c, T=T, theta=theta_list)
         ge_list = phonon_emiss(v=L_list, A=a, v_c=v_c, T=T, theta=theta_list)
 
+        # Inference the NN for predictions over the entire time domain
         s_pred = pinn(t_physics)
         sx_pred, sy_pred, sz_pred = s_pred[:, 0:1], s_pred[:, 1:2], s_pred[:, 2:3]
 
+        # Compute time derivatives of the predictions
         dsx_dt = torch.autograd.grad(sx_pred, t_physics, torch.ones_like(sx_pred), create_graph=True)[0]
         dsy_dt = torch.autograd.grad(sy_pred, t_physics, torch.ones_like(sy_pred), create_graph=True)[0]
         dsz_dt = torch.autograd.grad(sz_pred, t_physics, torch.ones_like(sz_pred), create_graph=True)[0]
 
+        # Calculate the mean (physics) loss as the difference between lhs and rhs of the differential equations squared
         loss_sx = torch.mean((dsx_dt + rabi_list*(ga_list-ge_list)/L_list + 
                               (detuning_list**2 + 2*rabi_list**2)*(ga_list+ge_list)*sx_pred/(2*L_list**2) +
                               detuning_list*sy_pred - detuning_list*rabi_list*(ga_list+ge_list)*sz_pred/(2*L_list**2))**2)
@@ -53,11 +80,14 @@ def train_inv_pinn_1(A0, v_c, T, D, Om_0, t_obs, sx_obs, sy_obs,
 
         loss_p = loss_sx + loss_sy + loss_sz
 
+        # Inference the NN for predictions over the observation time instances only
         s_obs_pred = pinn(t_obs)
         sx_obs_pred, sy_obs_pred, sz_obs_pred = s_obs_pred[:, 0:1], s_obs_pred[:, 1:2], s_obs_pred[:, 2:3]
 
+        # Calculate data loss as the mean loss between observed data and NN predictions squared
         loss_d = torch.mean((sx_obs - sx_obs_pred)**2 + (sy_obs - sy_obs_pred)**2 + (sz_obs - sz_obs_pred)**2)
 
+        # Total loss
         loss = loss_p + lambda1 * loss_d
         loss.backward()
         optimiser.step()
@@ -72,27 +102,53 @@ def train_inv_pinn_1(A0, v_c, T, D, Om_0, t_obs, sx_obs, sy_obs,
 
 
 
-#****************************************************************************************#
 
 
-def train_inv_pinn_2(v_c0, alpha, T, D, Om_0, t_obs, sx_obs, sy_obs, sz_obs, t_test, 
-                    t_intervals=150, hidden_dim=32, n_layers=4, lambda1=1e2, lr=1e-2,
-                    seed=2021, epochs=1001, plot_intervals=100):
+def train_inv_pinn_2(v_c0, alpha, T, D, Om_0, t_obs, sx_obs, sy_obs, sz_obs, t_test, t_intervals=150, 
+                     hidden_dim=32, n_layers=4, lambda1=1e2, lr=1e-2, seed=2021, epochs=1001, plot_intervals=100):
+
+    """
+    Estimate the value of cutoff frequency 'v_c_pred' (in 1/ps units) from the given 
+    noisy dataset, differential equations and initial boundary conditions.
+
+    Args:
+      v_c0: actual known value of cutoff frequency (1/ps)
+      alpha: coupling strength (dimensionless)
+      T: bath temperature (K)
+      D: pulse duration (ps)
+      t_obs: time instances of the observed noisy dataset (ps)
+      sx_obs, sy_obs, sz_obs: noisy dataset
+      t_test: time points over the entire domain [0,D] for inferencing
+      t_intervals: # time points between [0,D]
+      hidden_dim: # units per hidden layer
+      n_layers: # hidden layers
+      lambda1: regularization hyperparamter
+      lr: learning rate
+      seed: for reproducibility of results
+      epochs: # training iterations
+      plot_intervals: frequency of displaying learning plots
+
+    Returns:
+      v_c_pred, pinn: predicted value of cutoff frequency, trained pinn model
+    """
+    
     
     torch.manual_seed(seed)
     eps = 1e-10
-
+                      
+    # define PINN and additional learnable parameter 'v_c_pred' to learn the bath cutoff frequency (in 1/ps units)
     pinn = BlochNN(1, 3, hidden_dim, n_layers)
-    t_physics = torch.linspace(0, D, t_intervals).view(-1, 1).requires_grad_(True)
+    v_c_pred = torch.nn.Parameter(5 * torch.ones(1, requires_grad=True))      # initialized to 5 1/ps
 
-    
-    v_c_pred = torch.nn.Parameter(5 * torch.ones(1, requires_grad=True))
+    t_physics = torch.linspace(0, D, t_intervals).view(-1, 1).requires_grad_(True)
+                      
     optimiser = torch.optim.Adam(list(pinn.parameters()) + [v_c_pred], lr=lr)
     v_c_list = []
 
     for i in range(epochs):
         optimiser.zero_grad()
 
+        # Compute the time dependent parameters over the entire time domain
         rabi_list = rabi_freq(t=t_physics, D=D, Om_0=Om_0)
         detuning_list = detuning(t=t_physics, D=D, Om_0=Om_0)
         L_list = torch.sqrt(rabi_list ** 2 + detuning_list ** 2) + eps
@@ -100,13 +156,16 @@ def train_inv_pinn_2(v_c0, alpha, T, D, Om_0, t_obs, sx_obs, sy_obs, sz_obs, t_t
         ga_list = phonon_abs_1(v=L_list, theta=theta_list, v_c=v_c_pred, alpha=alpha, T=T)
         ge_list = phonon_emiss_1(v=L_list, theta=theta_list, v_c=v_c_pred, alpha=alpha, T=T)
 
+        # Inference the NN for predictions over the entire time domain
         s_pred = pinn(t_physics)
         sx_pred, sy_pred, sz_pred = s_pred[:, 0:1], s_pred[:, 1:2], s_pred[:, 2:3]
 
+        # Compute time derivatives of the predictions
         dsx_dt = torch.autograd.grad(sx_pred, t_physics, torch.ones_like(sx_pred), create_graph=True)[0]
         dsy_dt = torch.autograd.grad(sy_pred, t_physics, torch.ones_like(sy_pred), create_graph=True)[0]
         dsz_dt = torch.autograd.grad(sz_pred, t_physics, torch.ones_like(sz_pred), create_graph=True)[0]
 
+        # Calculate the mean (physics) loss as the difference between lhs and rhs of the differential equations squared
         loss_sx = torch.mean((dsx_dt + rabi_list * (ga_list - ge_list) / L_list +
                 (detuning_list ** 2 + 2 * rabi_list ** 2) * (ga_list + ge_list) * sx_pred / (2 * L_list ** 2) +
                 detuning_list * sy_pred - detuning_list * rabi_list * (ga_list + ge_list) * sz_pred / (2 * L_list ** 2)) ** 2)
@@ -119,13 +178,15 @@ def train_inv_pinn_2(v_c0, alpha, T, D, Om_0, t_obs, sx_obs, sy_obs, sz_obs, t_t
 
         loss_p = torch.mean(loss_sx + loss_sy + loss_sz)
 
-
+      
+        # Inference the NN for predictions over the observation time instances only
         s_obs_pred = pinn(t_obs)
         sx_obs_pred, sy_obs_pred, sz_obs_pred = s_obs_pred[:, 0:1], s_obs_pred[:, 1:2], s_obs_pred[:, 2:3]
 
-
+        # Calculate data loss as the mean loss between observed data and NN predictions squared
         loss_d = torch.mean((sx_obs - sx_obs_pred) ** 2 + (sy_obs - sy_obs_pred) ** 2 + (sz_obs - sz_obs_pred) ** 2)
 
+        # Total loss
         loss = loss_p + lambda1 * loss_d
         loss.backward()
         optimiser.step()
